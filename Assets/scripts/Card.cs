1fdb57aa47b3dcc04b9612a314b0dedadcaff213
James Iwamasa <jiwamasa@ucsc.edu>
2017-09-05 19:51:04 -0700

    Set up for some scripts.
    Created 'Card' and 'Deck' skeleton scripts.

---
+++ Assets/scripts/Card.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// defines card behaviour
+// card text is stored as child game objects
+public class Card : MonoBehaviour {
+	bool face_up; // is card face up?
+	GameObject owner; // owner of the card (null if no owner)
+
+	// flip the card (toggles face_up)
+	public void Flip() {
+		if (face_up) {
+			// FLIP FACE DOWN	
+		} else {
+			// FLIP FACE UP
+		}
+	}
+
+	// claims ownership of card
+	public void Claim() {
+
+	}
+
+}
---
+++ Assets/scripts/Deck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// defines deck behaviour
+// cards in deck are stored as child game objects
+public class Deck : MonoBehaviour {
+
+	// draw a card from the deck (face down)
+	public void Draw() {
+
+	}
+
+	// shuffle the deck
+	public void Shuffle() {
+
+	}
+
+}
